e3c6cd2b4df5f314be82a38a7d915bc9f4faf564
jonathan.aragon <jonathan.aragon@MU-C1-15.mullen.pvt.k12.co.us>
2022-03-04 13:35:00 -0700

    Friday Work

--- TimeAndTimeAgain/src/TimeAndTimeAgain.java
+++ TimeAndTimeAgain/src/TimeAndTimeAgain.java
@@ -1,16 +1,27 @@
-public class TimeAndTimeAgain {
+import java.util.*;
+import java.util.regex.Pattern;
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
-		// Format needed HH:MM:SS
-		// Read in input
-		// First number is the number of items
-		
-		// Loop through content
-		
-		
-		// Print out the numbers in appropriate format
+public class TimeAndTimeAgain
+	{
+		private static Scanner input = new Scanner(System.in);
+		private static Pattern secondRegex = Pattern.compile(".*\\d+)s.*");
+		private static Scanner intGetter = new Scanner(System.in);
+
+		public static void main(String[] args)
+			{
+				System.out.println("Hello World");
+				int numTimeCases = intGetter.nextInt();
+				// Format needed HH:MM:SS
+				// Read in input
+				// First number is the number of items
+
+				// Loop through content
+				for (int i = 0; i < numTimeCases; i++)
+					{
+						
+					}
+
+				// Print out the numbers in appropriate format
+			}
+
 	}
-
-}
